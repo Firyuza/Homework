b42d8ec9e53c392f276927722a88806897ef3280
Firyuza <shigapovafz@gmail.com>
2014-03-22 17:34:30 +0400

    Added interface Matrix

---
+++ MyMath/RunnerClass.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Фирюза on 15.03.14.
+ */
+import ru.kpfu.npp.math.MatrixArrayImpl;
+
+public class RunnerClass {
+    public static void main(String args[]) {
+
+        MatrixArrayImpl matrix;
+        matrix = new MatrixArrayImpl();
+        for (int i = 0; i < 30; i++) {
+            for (int j = 0; j < 30 ; j++) {
+                matrix.setValue(i, j, i + j);
+            }
+        }
+        System.out.println(matrix);
+    }
+ }
---
+++ MyMath/ru/kpfu/npp/math/Matrix.java
@@ -0,0 +1,10 @@
+package ru.kpfu.npp.math;
+
+/**
+ * Created by Фирюза on 15.03.14.
+ */
+interface Matrix {
+
+    public int getValue(int row, int column);
+    public void setValue(int row, int column, int value);
+}
